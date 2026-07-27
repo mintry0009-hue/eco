1f1ddd5aa9d02d69218060777aabeae9054c05ed
producer-mintry <mintry0009@gmail.com>
2026-07-27 13:29:35 +0900

    xxxxx

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 import requests
 import json
-url = "https://apis.data.go.kr/B552584/ArpltnInforInqireSvc/getMsrstnAcctoRltmMesureDnsty?serviceKey=f341b41a75fff847a9043475edd6be9cc7691c5740449a41bfd4d7ba03e9655a&returnType=json&numOfRows=1&pageNo=1&stationName=%EB%B0%B1%EC%84%9D%EB%8F%99&dataTerm=DAILY&ver=1.0"
+url_B = "https://apis.data.go.kr/B552584/ArpltnInforInqireSvc/getMsrstnAcctoRltmMesureDnsty?serviceKey=f341b41a75fff847a9043475edd6be9cc7691c5740449a41bfd4d7ba03e9655a&returnType=json&numOfRows=1&pageNo=1&stationName=%EB%B0%B1%EC%84%9D%EB%8F%99&dataTerm=DAILY&ver=1.0"
+url_ = ""
 
 params ={'serviceKey' : 'f341b41a75fff847a9043475edd6be9cc7691c5740449a41bfd4d7ba03e9655a', 'returnType' : 'json', 'numOfRows' : '1', 'pageNo' : '1', 'searchDate' : '2026-07-27', 'InformCode' : 'NO2' }
-req = requests.get(url, params=params)
+req = requests.get(url_B, params=params)
 
 j = req.json()
 json_output = json.dumps(j)
@@ -11,3 +12,4 @@ jToDict = json.loads(json_output)
 
 print(jToDict['response']['body']['items'][0]['dataTime'])
 print(jToDict['response']['body']['items'][0]['no2Value'])
+print(jToDict['response']['body']['items'][0]['pm25Value'])
